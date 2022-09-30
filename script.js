2ed1b698a4aec1c706aa4f34d9ab5839ff826dec
Ben Iverson <2513iverson989@gmail.com>
2022-09-30 12:10:22 -0500

    created 4 basic functions for math

---
+++ script.js
@@ -0,0 +1,19 @@
+//addition
+function add(a, b) {
+  return a + b;
+}
+
+//subtraction
+function subtract(a, b) {
+  return a - b;
+}
+
+//multiplication
+function multiply(a, b) {
+  return a * b;
+}
+
+//division
+function divide(a, b) {
+  return a / b;
+}
