3ae5c9e134564e86b91bc38df5dd06dc449ef327
Ben Iverson <2513iverson989@gmail.com>
2022-10-02 11:16:29 -0500

    completed  'operate' and tested each case

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 //addition
-function add(a, b) {
-  return a + b;
+function add(num1, num2) {
+  return num1 + num2;
 }
 
 //subtraction
-function subtract(a, b) {
-  return a - b;
+function subtract(num1, num2) {
+  return num1 - num2;
 }
 
 //multiplication
-function multiply(a, b) {
-  return a * b;
+function multiply(num1, num2) {
+  return num1 * num2;
 }
 
 //division
-function divide(a, b) {
-  return a / b;
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+//operate
+function operate(num1, sign, num2) {
+  if (sign === "+") {
+    return add(num1, num2);
+  }
+  if (sign === "-") {
+    return subtract(num1, num2);
+  }
+  if (sign === "*") {
+    return multiply(num1, num2);
+  }
+  if (sign === "/") {
+    return divide(num1, num2);
+  }
 }
