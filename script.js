7015063ef42ce660c967b82c7585ec81d049cb93
Ben Iverson <2513iverson989@gmail.com>
2023-02-06 19:08:54 -0600

    changed "sign" to "operator" to avoid confusion

--- script.js
+++ script.js
@@ -14,17 +14,21 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
-function operate(num1, sign, num2) {
-  if (sign === "+") {
+function operate(num1, operator, num2) {
+  if (operator === "+") {
     return add(num1, num2);
   }
-  if (sign === "-") {
+  if (operator === "-") {
     return subtract(num1, num2);
   }
-  if (sign === "*") {
+  if (operator === "*") {
     return multiply(num1, num2);
   }
-  if (sign === "/") {
+  if (operator === "/") {
     return divide(num1, num2);
   }
+}
+
+function () {
+  
 }
